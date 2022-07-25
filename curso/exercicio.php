7fce4152a7c848e70780381aade8ef04c47a1dab
braga-git <98855496+braga-git@users.noreply.github.com>
2022-07-25 20:07:23 -0300

    Update

---
+++ curso/exercicio.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+
+    <title>Exercício</title>
+
+</head>
+
+<body class="exercicio">
+    
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y') ?>
+    </footer>
+
+</body>
+
+</html>
--- curso/index.php
+++ curso/index.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="assets/css/style.css">
 
     <title>Curso PHP</title>
@@ -30,7 +31,7 @@
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="exercicio.php">Exercício A</a></li>
                         <li><a href="#">Exercício B</a></li>
                         <li><a href="#">Exercício C</a></li>
                     </ul>
@@ -104,7 +105,7 @@
     </main>
 
     <footer class="rodape">
-        COD3R & ALUNOS © 2022
+        COD3R & ALUNOS © <?= date('Y') ?>
     </footer>
 
 </body>
