fffc0d245cab599d0019c9312c12a0c69f836a26
Gabriel Braga <98855496+braga-git@users.noreply.github.com>
2022-09-05 18:03:27 -0300

    update

---
+++ curso/repeticoes/desafio_tabela.php
@@ -0,0 +1,48 @@
+<div class="titulo">Desafio Tabela</div>
+
+<?php
+    $matriz = [
+        ['01', '02', '03', '04', '05'],
+        ['06', '07', '08', '09', '10'],
+        ['11', '12', '13', '14', '15'],
+        ['16', '17', '18', '19', '20'],
+    ];
+
+    foreach($matriz as $linha){
+        foreach($linha as $valor){
+             echo "$valor";
+        }
+        echo '<br>';
+    }
+
+    echo '<br>';
+
+?>
+
+<table>
+    <?php
+        foreach($matriz as $linha){
+            echo '<tr>';
+            foreach($linha as $valor){
+                echo "<td> $valor </td>";
+            }
+            echo '</tr>';
+        }
+    ?>
+</table>
+
+<style>
+    table {
+        border: 1px solid #444;
+        border-collapse: collapse;
+    }
+
+    table tr {
+        border: 1px solid #444;
+    }
+
+    table td {
+        border: 1px solid #444;
+    }
+</style>
+
