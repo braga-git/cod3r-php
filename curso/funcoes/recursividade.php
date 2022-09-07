996b8be1e2eccaab0fbfd26a4ea8c09ae12f0831
braga-git <98855496+braga-git@users.noreply.github.com>
2022-09-07 14:28:01 -0300

    update

---
+++ curso/funcoes/recursividade.php
@@ -0,0 +1,36 @@
+<div class="titulo">Recursividade</div>
+
+<?php
+
+function somaUmAte($numero) {
+    $soma = 0;
+    for(; $numero >= 1; $numero--){
+        $soma += $numero;
+    } return $soma;
+} 
+
+echo somaUmAte(5) . '<br>';
+
+function somaUmAte2($numero) {
+    $soma = 0;
+    for($i = 1; $i <= $numero; $i++){
+        $soma += $i;
+    } return $soma;
+} 
+
+echo somaUmAte(5) . '<br>';
+
+function somaRecursivaUmAte($numero){
+    // condição de parada
+    if($numero === 1) {
+        return 1;
+    } return $numero + somaRecursivaUmAte($numero - 1);
+}
+
+echo somaRecursivaUmAte(5) . '<br>';
+
+function somaRecursivaEconomica($numero){
+    return $numero === 1 ? 1 : $numero + somaRecursivaEconomica($numero - 1);
+}
+
+echo somaRecursivaEconomica(5) . '<br>';
